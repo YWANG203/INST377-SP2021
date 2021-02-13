29041e01641edefb7318be0a554d8984372b13bf
YiyangWang <49288220+YWANG203@users.noreply.github.com>
2021-02-13 09:01:55 +0800

    lab_3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,74 @@
-/* Put your javascript in here */
+const carouselContainer = document.querySelector(".carousel-container");
+const listImageArea = carouselContainer.querySelector(".next-list");
+const listOfImages = listImageArea.querySelectorAll("img");
+const currentImage = carouselContainer.querySelector(".current-image");
+const arrowLeft = carouselContainer.querySelector(".arrow-left");
+const arrowRight = carouselContainer.querySelector(".arrow-right");
+
+function styleList() {
+    if (listImageArea.scrollWidth == listImageArea.offsetWidth) {
+        listImageArea.style.justifyContent = "center";
+    } else {
+        listImageArea.style.justifyContent = "flex-start";
+    }
+}
+
+function goToRight() {
+    var current = listImageArea.querySelector(".current-image-list");
+    current.parentElement.nextElementSibling.children[0].classList.add(
+        "current-image-list"
+    );
+    current.classList.remove("current-image-list");
+    current = listImageArea.querySelector(".current-image-list");
+    listImageArea.scrollLeft = current.offsetLeft;
+    currentImage.attributes.src.value = current.attributes.src.value;
+    currentImage.classList.add("slideInFromRight");
+    setTimeout(() => {
+        currentImage.classList.remove("slideInFromRight");
+    }, 500);
+}
+
+function goToLeft() {
+    var current = listImageArea.querySelector(".current-image-list");
+    current.parentElement.previousElementSibling.children[0].classList.add(
+        "current-image-list"
+    );
+    current.classList.remove("current-image-list");
+    current = listImageArea.querySelector(".current-image-list");
+    listImageArea.scrollLeft = current.offsetLeft;
+    currentImage.attributes.src.value = current.attributes.src.value;
+    currentImage.classList.add("slideInFromLeft");
+    setTimeout(() => {
+        currentImage.classList.remove("slideInFromLeft");
+    }, 500);
+}
+
+function changeCurrentImage(newImage) {
+    currentImage.classList.add("fadeIn");
+    setTimeout(() => {
+        currentImage.classList.remove("fadeIn");
+    }, 500);
+    currentImage.attributes.src.value = this.attributes.src.value;
+    listOfImages.forEach((image) => {
+        image.classList.remove("current-image-list");
+    });
+    this.classList.add("current-image-list");
+}
+
+styleList();
+
+arrowLeft.addEventListener("click", goToLeft);
+arrowRight.addEventListener("click", goToRight);
+
+window.addEventListener("resize", (e) => {
+    styleList();
+});
+
+(function() {
+    if (typeof NodeList.prototype.forEach === "function") return false;
+    NodeList.prototype.forEach = Array.prototype.forEach;
+})();
+
+listOfImages.forEach((image) => {
+    image.addEventListener("click", changeCurrentImage);
+});
