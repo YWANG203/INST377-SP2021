b885094b76468c4198c7ca2fab9c5710d6795692
YiyangWang <49288220+YWANG203@users.noreply.github.com>
2021-02-20 01:25:11 +0800

    lab 4

--- public/labs/lab_4/script.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.all('/secret', function(req, res, next) {
-    console.log('Accessing the secret section ...')
-    next()
-})
-app.all('*', requireAuthentication, loadUser)
